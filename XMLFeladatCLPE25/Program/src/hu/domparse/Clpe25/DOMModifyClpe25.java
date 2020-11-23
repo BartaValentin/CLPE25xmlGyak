98d25449e7beb171a1b5f09e7ca12768cd30eb97
Barta Valentin <bartavalentin99@gmail.com>
2020-11-23 12:07:58 +0100

    féléves feladat

---
+++ XMLFeladatCLPE25/Program/src/hu/domparse/Clpe25/DOMModifyClpe25.java
@@ -0,0 +1,423 @@
+package hu.domparse.Clpe25;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.util.Scanner;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+public class DOMModifyClpe25 {
+
+	public static String ReadId() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nid:");
+		String id = sc.nextLine();
+		return id;
+	}
+	
+	public static void CreateXML(Document doc) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("src/hu/domparse/Clpe25/XMLCLPE25.updated.xml"));
+        transformer.transform(source, result);
+	}
+	
+	public static void UpdatePlayer(Document doc) throws TransformerException {
+
+		System.out.println("\nMelyik játékost szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadPlayer(doc);
+
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Jatékos neve: ");
+		String name = sc.nextLine();
+		System.out.print("Jatékos beceneve: ");
+		String nickname = sc.nextLine();
+		System.out.print("Jatékos csapata: ");
+		String team = sc.nextLine();
+		System.out.print("Bajnokság: ");
+		String league = sc.nextLine();
+		System.out.print("Orszag: ");
+		String country = sc.nextLine();
+		
+		UpdatePlayerById(doc, id, name, nickname, team, league, country);
+	}
+
+	public static void UpdatePlayerById(Document doc, String id, String name, String nickname, String team, String league, String country) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("player");
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String playerid = element.getAttribute("id");
+
+				if (playerid.equals(id)) {
+
+					String sponsorid = element.getAttribute("sponsorid");
+					String matchid = element.getAttribute("matchid");
+					String positionid = element.getAttribute("positionid");
+
+					Node node1 = element.getElementsByTagName("name").item(0);
+					node1.setTextContent(name);
+
+					Node node2 = element.getElementsByTagName("nickname").item(0);
+					node2.setTextContent(nickname);
+					
+					Node node3 = element.getElementsByTagName("team").item(0);
+					node3.setTextContent(team);
+					
+					Node node4 = element.getElementsByTagName("country").item(0);
+					node4.setTextContent(country);
+					
+					Node node5 = element.getElementsByTagName("league").item(0);
+					node5.setTextContent(league);
+					
+					System.out.println("Jatekos Id:" + playerid + "\tSzponzor id:" + sponsorid + "\tMérközés id:"
+							+ matchid + "\tPozício Id:" + positionid + "\tNev: " + node1.getTextContent() + "\tBecenev: " + node2.getTextContent()
+							+ "\tCsapat: " + node3.getTextContent() + "\tOrszag:" + node4.getTextContent() + "\tLiga: " + node5.getTextContent());
+
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+			}
+		}
+		
+		CreateXML(doc);
+	}
+	
+	public static void UpdateSponsor(Document doc) throws TransformerException {
+	
+		System.out.println("\nMelyik szponzort szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadSponsor(doc);
+
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Név: ");
+		String name = sc.nextLine();
+		System.out.print("Ország: ");
+		String country = sc.nextLine();
+		System.out.print("Bevétel: ");
+		String income = sc.nextLine();
+
+		UpdateSponsorById(doc, id, name, country, income);
+		
+	}
+
+	public static void UpdateSponsorById(Document doc, String id, String name, String country, String income) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("sponsor");
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String sponsorid = element.getAttribute("id");
+
+				if (sponsorid.equals(id)) {
+
+					Node node1 = element.getElementsByTagName("name").item(0);
+					node1.setTextContent(name);
+
+					Node node2 = element.getElementsByTagName("country").item(0);
+					node2.setTextContent(country);
+					
+					Node node3 = element.getElementsByTagName("income").item(0);
+					node3.setTextContent(income);
+					
+					System.out.println("Szponzor id:" + sponsorid + "\tNev: " + node1.getTextContent() + "\tOrszag: " + node2.getTextContent()
+							+ "\tBevetel: " + node3.getTextContent());
+					
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+			}
+		}
+		
+		CreateXML(doc);
+	}
+	
+	public static void UpdateMatch(Document doc) throws TransformerException {
+
+		System.out.println("\nMelyik mérközést szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadMatch(doc);
+		
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Helyszín: ");
+		String location = sc.nextLine();
+		System.out.print("Végeredmény: ");
+		String fulltime = sc.nextLine();
+		System.out.print("Félidõ: ");
+		String halftime = sc.nextLine();
+		System.out.print("Bíró: ");
+		String referee = sc.nextLine();
+		System.out.print("Év: ");
+		String year = sc.nextLine();
+		System.out.print("Hónap: ");
+		String month = sc.nextLine();
+		System.out.print("Nap: ");
+		String day = sc.nextLine();
+	
+		UpdateMatchById(doc, id, location, fulltime, halftime, referee, year, month, day);
+		
+	}
+
+	public static void UpdateMatchById(Document doc, String id, String location, String fulltime, String halftime, String referee, String year, String month, String day) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("match");
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String sponsorid = element.getAttribute("id");
+
+				if (sponsorid.equals(id)) {
+
+					Node node1 = element.getElementsByTagName("location").item(0);
+					node1.setTextContent(location);
+
+					Node node2 = element.getElementsByTagName("fulltime").item(0);
+					node2.setTextContent(fulltime);
+					
+					Node node3 = element.getElementsByTagName("halftime").item(0);
+					node3.setTextContent(halftime);
+					
+					Node node4 = element.getElementsByTagName("referee").item(0);
+					node4.setTextContent(referee);
+										
+					for (int j = 0; j < cList.getLength(); j++) {
+
+						Node cnode1 = element.getElementsByTagName("year").item(0);
+						cnode1.setTextContent(year);
+
+						Node cnode2 = element.getElementsByTagName("month").item(0);
+						cnode2.setTextContent(month);
+
+						Node cnode3 = element.getElementsByTagName("day").item(0);
+						cnode3.setTextContent(day);
+
+					}
+
+					System.out.println("Merkozes id:" + id + "\tHelyszin: " + node1.getTextContent() + "\tVegeredmeny: " + node2.getTextContent()
+							+ "\tFelido: " + node3.getTextContent() + "\tBiro: " + node4.getTextContent() + "\tDatum: " + year + "." + month + "."
+							+ day);
+										
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+			}
+		}
+		
+		CreateXML(doc);
+	}
+	
+	public static void UpdatePosition(Document doc) throws TransformerException {
+
+		System.out.println("\nMelyik pozíciót szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadPosition(doc);
+
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Név: ");
+		String name = sc.nextLine();
+	
+		UpdatePositionById(doc, id, name);
+		
+	}
+
+	public static void UpdatePositionById(Document doc, String id, String name) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("position");
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String positionid = element.getAttribute("id");
+
+				if (positionid.equals(id)) {
+
+					Node node1 = element.getElementsByTagName("name").item(0);
+					node1.setTextContent(name);
+				
+					System.out.println("Pozicio id:" + positionid + "\tNev: " + name);
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+								
+			}
+		}
+		
+		CreateXML(doc);
+	}
+	
+	public static void UpdateTeam(Document doc) throws TransformerException {
+
+		System.out.println("\nMelyik csapatot szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadTeam(doc);
+		
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Név: ");
+		String name = sc.nextLine();
+		System.out.print("Ország: ");
+		String country = sc.nextLine();
+		System.out.print("Város: ");
+		String city = sc.nextLine();
+		
+		UpdateTeamById(doc, id, name, country, city);
+		
+	}
+
+	public static void UpdateTeamById(Document doc, String id, String name, String country, String city) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("footballteam");
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String teamid = element.getAttribute("id");
+
+				if (teamid.equals(id)) {
+
+					Node node1 = element.getElementsByTagName("name").item(0);
+					node1.setTextContent(name);
+
+										
+					for (int j = 0; j < cList.getLength(); j++) {
+
+						Node cnode1 = element.getElementsByTagName("country").item(0);
+						cnode1.setTextContent(country);
+
+						Node cnode2 = element.getElementsByTagName("city").item(0);
+						cnode2.setTextContent(city);
+
+					}
+
+					System.out.println("Csapat id: " + teamid + "\tNev: " + node1.getTextContent() + "\tOrszag: " + country + "\tVaros: " + city);
+					
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+			}
+		}
+		
+		CreateXML(doc);
+	}
+	
+	public static void UpdateContract(Document doc) throws TransformerException {
+
+		System.out.println("\nMelyik szerzõdést szeretné módosítani?\n");
+
+		DOMReadClpe25.ReadContract(doc);
+		
+		String id = ReadId();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Leírás: ");
+		String description = sc.nextLine();
+		System.out.print("Év: ");
+		String year = sc.nextLine();
+		System.out.print("Hónap: ");
+		String month = sc.nextLine();
+		System.out.print("Nap: ");
+		String day = sc.nextLine();
+		
+		UpdateContractById(doc, id, description, year, month, day);
+	}
+	
+	public static void UpdateContractById(Document doc, String id, String description, String year, String month, String day) throws TransformerException {
+
+		NodeList nList = doc.getElementsByTagName("contract");
+				
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String contractid = element.getAttribute("id");
+
+				if (contractid.equals(id)) {
+
+					Node node1 = element.getElementsByTagName("description").item(0);
+					node1.setTextContent(description);
+				
+					for (int j = 0; j < cList.getLength(); j++) {
+
+						Node cnode1 = element.getElementsByTagName("year").item(0);
+						cnode1.setTextContent(year);
+
+						Node cnode2 = element.getElementsByTagName("month").item(0);
+						cnode2.setTextContent(month);
+
+						Node cnode3 = element.getElementsByTagName("day").item(0);
+						cnode3.setTextContent(day);
+						
+					}
+					
+					System.out.println("Szerzodes id:\t" + contractid + "\tDatum:\t" + year + "." + month + "." + day + "\tLeiras:\t" + node1.getTextContent() );
+					System.out.println("\nSikeres Modosítas\n");
+					
+				}
+								
+			}
+		}
+		
+		CreateXML(doc);
+	}
+
+}
---
+++ XMLFeladatCLPE25/Program/src/hu/domparse/Clpe25/DOMReadClpe25.java
@@ -0,0 +1,365 @@
+package hu.domparse.Clpe25;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DOMReadClpe25 {
+
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+
+	       try {
+	   		File xmlFile = new File("src/hu/domparse/Clpe25/XMLCLPE25.xml");
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
+			doc.getDocumentElement().normalize();		
+			System.out.println("Barta Valentin CLPE25 XML beadando");
+			Action(doc);	   
+	       }catch (ParserConfigurationException pce) {
+	           pce.printStackTrace();
+	       } catch (TransformerException tfe) {
+	        tfe.printStackTrace();
+	       } catch (IOException ioe) {
+	        ioe.printStackTrace();
+	       } catch (SAXException sae) {
+	        sae.printStackTrace();
+	       }
+	}
+
+	public static void Action(Document doc) throws TransformerException {
+		System.out.println("\nOlvasni vagy modosítani szeretne?");
+		System.out.println("1 olvasas");
+		System.out.println("2 modositas");
+		int action = ReadCategory();
+
+		switch (action) {
+		case 1:
+			Read(doc);
+			break;
+		case 2:
+			Update(doc);
+			break;
+		default:
+			Action(doc);
+			break;
+		}
+
+	}
+
+	public static int ReadCategory() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("\nAdja meg a sorszamot:");
+		int readCategory = scan.nextInt();
+		return readCategory;
+	}
+	
+	public static void Update(Document doc) throws TransformerException {
+		System.out.println("\nXML Módosítas\n");
+		System.out.println("Kerem adja meg mit szeretne modositani");
+		System.out.println("1 jatekos\n2 szponzor\n3 merkozes\n4 pozicio\n5 csapat\n6 szerzodes");
+		int category = 0;
+		category = ReadCategory();
+		ShowElementUpdates(category, doc);
+	}
+	
+	public static void Read(Document doc) {
+		System.out.println("\nXML Olvasas\n");
+		System.out.println("Kerem adja meg mit szeretne olvasni");
+		System.out.println("1 jatekos\n2 szponzor\n3 merkozés\n4 pozicio\n5 csapat\n6 szerzodes");
+		int category = 0;
+		category = ReadCategory();
+		ShowCategoryElements(category, doc);
+	}
+
+	public static void ShowCategoryElements(int category, Document doc) {
+		switch (category) {
+		case 1:
+			ReadPlayer(doc);
+			break;
+		case 2:
+			ReadSponsor(doc);
+			break;
+		case 3:
+			ReadMatch(doc);
+			break;
+		case 4:
+			ReadPosition(doc);
+			break;
+		case 5:
+			ReadTeam(doc);
+			break;
+		case 6:
+			ReadContract(doc);
+			break;
+		default:
+			int newCategory = ReadCategory();
+			ShowCategoryElements(newCategory, doc);
+			break;
+		}
+	}
+
+	public static void ShowElementUpdates(int category, Document doc) throws TransformerException {
+		switch (category) {
+		case 1:
+			DOMModifyClpe25.UpdatePlayer(doc);
+			break;
+		case 2:
+			DOMModifyClpe25.UpdateSponsor(doc);
+			break;
+		case 3:
+			DOMModifyClpe25.UpdateMatch(doc);
+			break;
+		case 4:
+			DOMModifyClpe25.UpdatePosition(doc);
+			break;
+		case 5:
+			DOMModifyClpe25.UpdateTeam(doc);
+			break;
+		case 6:
+			DOMModifyClpe25.UpdateContract(doc);
+			break;
+		default:
+			int newCategory = ReadCategory();
+			ShowElementUpdates(newCategory, doc);
+			break;
+		}
+	}
+	
+	public static void ReadPosition(Document doc) {
+
+		NodeList nList = doc.getElementsByTagName("position");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String positionid = element.getAttribute("id");
+
+				Node node1 = element.getElementsByTagName("name").item(0);
+				String name = node1.getTextContent();
+
+				System.out.println("Pozicio id:" + positionid + "\tNev: " + name);
+			}
+		}
+
+	}
+
+	public static void ReadTeam(Document doc) {
+
+		NodeList nList = doc.getElementsByTagName("footballteam");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String teamid = element.getAttribute("id");
+
+				Node node1 = element.getElementsByTagName("name").item(0);
+				String name = node1.getTextContent();
+				
+				String country = "";
+				String city = "";
+
+				for (int j = 0; j < cList.getLength(); j++) {
+
+					Node cnode1 = element.getElementsByTagName("country").item(0);
+					country = cnode1.getTextContent();
+					
+					Node cnode2 = element.getElementsByTagName("city").item(0);
+					city = cnode2.getTextContent();
+
+				}
+				
+				System.out.println("Csapat id: " + teamid + "\tNev: " + name + "\tOrszag: " + country + "\tVaros: " + city);
+			}
+			
+		}
+	}
+
+	public static void ReadMatch(Document doc) {
+
+		NodeList nList = doc.getElementsByTagName("match");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String matchid = element.getAttribute("id");
+
+				Node node1 = element.getElementsByTagName("location").item(0);
+				String location = node1.getTextContent();
+
+				Node node2 = element.getElementsByTagName("fulltime").item(0);
+				String fulltime = node2.getTextContent();
+
+				Node node3 = element.getElementsByTagName("halftime").item(0);
+				String halftime = node3.getTextContent();
+
+				Node node4 = element.getElementsByTagName("referee").item(0);
+				String referee = node4.getTextContent();
+
+				String year = "";
+				String month = "";
+				String day = "";
+
+				for (int j = 0; j < cList.getLength(); j++) {
+
+					Node cnode1 = element.getElementsByTagName("year").item(0);
+					year = cnode1.getTextContent();
+
+					Node cnode2 = element.getElementsByTagName("month").item(0);
+					month = cnode2.getTextContent();
+
+					Node cnode3 = element.getElementsByTagName("day").item(0);
+					day = cnode3.getTextContent();
+
+				}
+
+				System.out.println("Merkozes id:" + matchid + "\tHelyszin: " + location + "\tVegeredmeny: " + fulltime
+						+ "\tFelido: " + halftime + "\tBiro: " + referee + "\tDatum: " + year + "." + month + "."
+						+ day);
+			}
+		}
+	}
+
+	public static void ReadSponsor(Document doc) {
+
+		NodeList nList = doc.getElementsByTagName("sponsor");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String sponsorid = element.getAttribute("id");
+
+				Node node1 = element.getElementsByTagName("name").item(0);
+				String name = node1.getTextContent();
+
+				Node node2 = element.getElementsByTagName("country").item(0);
+				String country = node2.getTextContent();
+
+				Node node3 = element.getElementsByTagName("income").item(0);
+				String income = node3.getTextContent();
+
+				System.out.println("Szponzor id:" + sponsorid + "\tNev: " + name + "\tOrszag: " + country
+						+ "\tBevetel: " + income);
+			}
+		}
+	}
+
+	public static void ReadPlayer(Document doc) {
+
+		NodeList nList = doc.getElementsByTagName("player");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String playerid = element.getAttribute("id");
+				String sponsorid = element.getAttribute("sponsorid");
+				String matchid = element.getAttribute("matchid");
+				String positionid = element.getAttribute("positionid");
+
+				Node node1 = element.getElementsByTagName("name").item(0);
+				String name = node1.getTextContent();
+
+				Node node2 = element.getElementsByTagName("nickname").item(0);
+				String nickname = node2.getTextContent();
+
+				Node node3 = element.getElementsByTagName("team").item(0);
+				String team = node3.getTextContent();
+
+				Node node4 = element.getElementsByTagName("country").item(0);
+				String country = node4.getTextContent();
+
+				Node node5 = element.getElementsByTagName("league").item(0);
+				String league = node5.getTextContent();
+
+				System.out.println("Jatekos Id:" + playerid + "\tSzponzor id:" + sponsorid + "\tMerkozes id:" + matchid
+						+ "\tPozicio Id:" + positionid + "\tNev: " + name + "\tBecenev: " + nickname + "\tCsapat: "
+						+ team + "\tOrszag:" + country + "\tLiga: " + league);
+
+			}
+		}
+
+	}
+
+	public static void ReadContract(Document doc) {
+		
+		NodeList nList = doc.getElementsByTagName("contract");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+
+			Node nNode = nList.item(i);
+
+			NodeList cList = nList.item(i).getChildNodes();
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) nNode;
+
+				String contractid = element.getAttribute("id");
+
+				Node node1 = element.getElementsByTagName("description").item(0);
+				String description = node1.getTextContent();
+
+				String year = "";
+				String month = "";
+				String day = "";
+
+				for (int j = 0; j < cList.getLength(); j++) {
+
+					Node cnode1 = element.getElementsByTagName("year").item(0);
+					year = cnode1.getTextContent();
+
+					Node cnode2 = element.getElementsByTagName("month").item(0);
+					month = cnode2.getTextContent();
+
+					Node cnode3 = element.getElementsByTagName("day").item(0);
+					day = cnode3.getTextContent();
+
+				}
+
+				System.out.println("Szerzodes id:\t" + contractid + "\tDatum:\t" + year + "." + month + "." + day + "\tLeiras:\t" + description );
+			}
+		}
+		
+	}
+	
+}
